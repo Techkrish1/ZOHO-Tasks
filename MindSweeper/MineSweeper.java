20442c8c9ab55ce85a1171ab25a8e34798f5d24f
Techkrish1 <krishgokul4002@gmail.com>
2024-09-10 15:27:20 +0530

    File Organized

---
+++ MindSweeper/MineSweeper.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class PlayGame{
+
+
+}
+
+class GameSetting{
+
+}
+
+public class MineSweeper {
+
+    public static void main(String[] args){
+        System.out.println("yes");
+    }
+
+}
